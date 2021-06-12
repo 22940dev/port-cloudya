0edcc1fb7ff6e027b210a9ebae15807cd0a953c3
yahya-cloud <59670962+yahya-cloud@users.noreply.github.com>
2021-06-12 23:00:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 
-import './App.css'
+
 import Footer from './components/Footer/Footer'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
         className='text__box'>
         <h1 className='text__box--greeting'>Hello;</h1>
         <h3 className='text__box--para'>
-          I'm Mohd. Yahya, <span>M</span> <span>E</span> <span>R</span>{' '}
+          I'm Yahya, <span>M</span> <span>E</span> <span>R</span>{' '}
           <span>N</span> stack developer looking for an Internship
         </h3>
         <a href={resume} download><button  className='text__box--button'>download CV</button></a>
